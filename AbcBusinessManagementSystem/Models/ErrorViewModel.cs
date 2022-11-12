7662b3972c676209d83fba82da2f8e59a9ad06a5
engineerdavut <68922691+engineerdavut@users.noreply.github.com>
2022-11-12 14:16:17 +0300

    Add project files.

---
+++ AbcBusinessManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbcBusinessManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
