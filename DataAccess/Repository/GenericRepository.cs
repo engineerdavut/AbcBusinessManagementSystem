e21c69d86c2097d91592b00a5c8284fafcfaef0f
engineerdavut <68922691+engineerdavut@users.noreply.github.com>
2022-11-12 17:29:49 +0300

    dataaccess abstract ,concrete genericrepository olusturuldu.

---
+++ DataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Add(T t)
+        {
+            using var context = new Context();
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public void Update(T t)
+        {
+            using var context = new Context();
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
